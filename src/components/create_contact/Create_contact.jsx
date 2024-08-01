351ab6728486ecaf5c33edc494522c6581827e73
Tazwar <142654711+Fahim-instaWebWorks@users.noreply.github.com>
2024-08-01 13:11:53 +0600

    okk

--- src/components/create_contact/Create_contact.jsx
+++ src/components/create_contact/Create_contact.jsx
@@ -4,19 +4,19 @@ import {
   Dialog,
   DialogActions,
   DialogContent,
-  DialogContentText,
   DialogTitle,
   Grid,
   IconButton,
-  Modal,
+  Input,
   TextField,
   Typography,
 } from "@mui/material";
-import React, { useCallback, useState } from "react";
+import React, { forwardRef, useEffect, useState } from "react";
 import CloseIcon from "@mui/icons-material/Close";
 import { Controller, useForm } from "react-hook-form";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
-import { usePlacesWidget } from "react-google-autocomplete";
+import ReactGoogleAutocomplete from "react-google-autocomplete";
+import usePlacesService from "react-google-autocomplete/lib/usePlacesAutocompleteService";
+
 
 const fields = [
   { label: "First Name", id: "firstName", type: "text" },
@@ -37,46 +37,33 @@ const fields = [
   { label: "Mailing Zip", id: "Mailing Zip", type: "number" },
 ];
 
-const containerStyle = {
-  width: "400px",
-  height: "400px",
-};
-
-const center = {
-  lat: -3.745,
-  lng: -38.523,
-};
 
 const Create_contact = ({ open, setOpen }) => {
-  // const [open, setOpen] = useState(false);
-  const [map, setMap] = useState(null);
-  const { handleSubmit, control, reset } = useForm();
-  const [country, setCountry] = useState("us");
-  const { ref: materialRef } = usePlacesWidget({
-    apiKey: "AIzaSyByaA2Kqxo7a3SBqaD97S8B5c41kggfycI",
-    onPlaceSelected: (place) => console.log(place),
-    inputAutocompleteValue: "country",
-    options: {
-      componentRestrictions: { country },
-    },
+  const { handleSubmit, control, reset, setValue } = useForm();
+  const [country] = useState("us");
+
+  const { placesService, placePredictions } = usePlacesService({
+    apiKey: process.env.REACT_APP_GOOGLE,
   });
+  const AutocompleteInput = forwardRef((props, ref) => (
+    <ReactGoogleAutocomplete
+      {...props}
+      ref={ref}
+    />
+  ));
 
-  // const { isLoaded } = useJsApiLoader({
-  //   id: "google-map-script",
-  //   googleMapsApiKey: "AIzaSyByaA2Kqxo7a3SBqaD97S8B5c41kggfycI",
-  // });
-
-  // const onLoad = useCallback(function callback(map) {
-  //   // This is just an example of getting and using the map instance!!! don't just blindly copy!
-  //   const bounds = new window.google.maps.LatLngBounds(center);
-  //   map.fitBounds(bounds);
-
-  //   setMap(map);
-  // }, []);
-
-  // const onUnmount = useCallback(function callback(map) {
-  //   setMap(null);
-  // }, []);
+  useEffect(() => {
+    if (placePredictions.length) {
+      placesService.getDetails(
+        {
+          placeId: placePredictions[0].place_id,
+        },
+        (placeDetails) => {
+          console.log(placeDetails);
+        }
+      );
+    }
+  }, [placePredictions, placesService]);
 
   const handleClose = () => {
     setOpen(false);
@@ -84,8 +71,32 @@ const Create_contact = ({ open, setOpen }) => {
 
   const onSubmit = (data) => {
     console.log(data);
-    //   handleClose();
     reset();
+    handleClose();
+  };
+
+  const extractAddressComponent = (addressComponents, type) => {
+    // console.log(addressComponents)
+    const component = addressComponents.find((component) =>
+      component.types.includes(type)
+    );
+    return component ? component.long_name : "";
+  };
+
+  const handlePlaceSelected = (place) => {
+    console.log(place);
+    const addressComponents = place.address_components;
+
+    const city = extractAddressComponent(addressComponents, "locality");
+    const state = extractAddressComponent(
+      addressComponents,
+      "administrative_area_level_1"
+    );
+    const zip = extractAddressComponent(addressComponents, "postal_code");
+    setValue("Mailing Address", place.formatted_address);
+    setValue("Mailing City", city);
+    setValue("Mailing State", state);
+    setValue("Mailing Zip", zip);
   };
 
   return (
@@ -121,7 +132,6 @@ const Create_contact = ({ open, setOpen }) => {
       </DialogTitle>
       <form onSubmit={handleSubmit(onSubmit)}>
         <DialogContent>
-          {/* <Box display="flex" flexDirection="column" gap={2} mt={1}> */}
           <Grid container columns={12} spacing={2}>
             {fields.map((field, index) => (
               <Grid
@@ -129,7 +139,7 @@ const Create_contact = ({ open, setOpen }) => {
                 item
                 xs={field.label === "Mailing Address" ? 12 : 6}
               >
-                <Box display="flex" alignItems="center" height={20} my={0.5}>
+                <Box display="flex" alignItems="center" height={20} my={0.5} p={0}>
                   <Typography variant="body1" sx={{ minWidth: "120px" }}>
                     {field.label}
                   </Typography>
@@ -139,15 +149,78 @@ const Create_contact = ({ open, setOpen }) => {
                     defaultValue=""
                     render={({ field: controllerField }) =>
                       field.label === "Mailing Address" ? (
-                        <TextField
-                          {...controllerField}
+                        <>
+                          {/* <Input
+                        
+                          fullWidth
                           margin="dense"
-                          type={field.type}
-                          variant="outlined"
+                          color="primary"
                           size="small"
-                          sx={{ ml: 2, flex: 1,bgcolor:'red' }}
-                          inputRef={materialRef}
-                        />
+                          disableUnderline
+                          
+                          sx={{border:'1px solid #C4C4C4' ,borderRadius:1,ml:2,flex:1,pl:0.8,height:35,paddingBlock:"1px",paddingInline:"2px","&:hover":{border:'1px solid black'}}}
+                          inputComponent={({
+                            inputRef,
+                            onFocus,
+                            onBlur,
+                            ...props
+                          }) => (
+                            <ReactGoogleAutocomplete
+                              apiKey=process.env.REACT_APP_GOOGLE
+                              {...props}
+                              onPlaceSelected={handlePlaceSelected}
+                              options={{
+                                types: ["address"],
+                                componentRestrictions: { country },
+                              }}
+                              defaultValue={controllerField.value}
+                              style={{ width: "100%" }}
+                            />
+                          )}
+                        /> */}
+                          {/* <TextField
+                            margin="dense"
+                            variant="outlined"
+                            size="small"
+                            sx={{ ml: 2, flex: 1 }}
+                            InputProps={{
+                              inputComponent: ({ inputRef, ...props }) => (
+                                <ReactGoogleAutocomplete
+                                  apiKey=process.env.REACT_APP_GOOGLE
+                                  {...props}
+                                  onPlaceSelected={handlePlaceSelected}
+                                  options={{
+                                    types: ["address"],
+                                    componentRestrictions: { country },
+                                  }}
+                                  style={{ width: "100%" }}
+                                  defaultValue={controllerField.value}
+                                />
+                              ),
+                            }}
+                          /> */}
+
+                          <TextField
+                            {...controllerField}
+                            margin="dense"
+                            variant="outlined"
+                            size="small"
+                            sx={{ ml: 2, flex: 1 }}
+                            InputProps={{
+                              inputComponent: AutocompleteInput,
+                              inputProps: {
+                                apiKey:process.env.REACT_APP_GOOGLE,
+                                onPlaceSelected: handlePlaceSelected,
+                                options: {
+                                  types: ["address"],
+                                  componentRestrictions: { country },
+                                },
+                                style: { width: "100%" },
+                                defaultValue: controllerField.value,
+                              },
+                            }}
+                          />
+                        </>
                       ) : (
                         <TextField
                           {...controllerField}
@@ -155,28 +228,15 @@ const Create_contact = ({ open, setOpen }) => {
                           type={field.type}
                           variant="outlined"
                           size="small"
-                          sx={{ ml: 2, flex: 1 }}
+                          sx={{ ml: 2, flex: 1,"& .MuiTextField-root":{padding:0}}}
                         />
                       )
                     }
                   />
                 </Box>
-                {/* {field.label === "Mailing Address" && (
-                  <GoogleMap
-                    mapContainerStyle={containerStyle}
-                    center={center}
-                    zoom={10}
-                    onLoad={onLoad}
-                    onUnmount={onUnmount}
-                  >
-                    
-                    <></>
-                  </GoogleMap>
-                )} */}
               </Grid>
             ))}
           </Grid>
-          {/* </Box> */}
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
